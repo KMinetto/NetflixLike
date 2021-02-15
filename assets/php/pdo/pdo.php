125dd22b738491290878b260d93c8a1e7edd288e
Kilian MINETTO <killian.minetto@gmail.com>
2021-02-15 17:26:40 +0100

    Add pdo connexion to adminer

--- assets/php/pdo/pdo.php
+++ assets/php/pdo/pdo.php
@@ -1,8 +1,8 @@
 <?php
 
-$dsn = "mysql:host=localhost;dbname=netflix_like;charset=utf8";
-$user = 'root';
-$password = '';
+$dsn = "mysql:host=https://kilianm.promo-45.codeur.online/adminer;dbname=kilianm_netflix_like;charset=utf8";
+$user = 'kilianm';
+$password = 'oUH50e5S1CD+vg==';
 $options = array(
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8;",
